155cc682c275b17b953ec223d5606c3d17b85639
midbar40 <jklhdd@naver.com>
2024-09-04 08:27:54 +0900

    vercel postgre로 db 변경

--- interior/src/app/api/db/admin/route.ts
+++ interior/src/app/api/db/admin/route.ts
@@ -1,9 +1,7 @@
 import { NextResponse } from 'next/server';
-import { connectMysql } from '@/app/api/db/connectDb'
-import { createTable } from './createAdminTable';
+import { sql } from '@vercel/postgres';
 import bcrypt from 'bcrypt'
 
-createTable().catch(err => console.error('Unhandled error:', err));
 
 // hash암호화 함수
 async function hashPassword(pw: string) {
@@ -17,75 +15,34 @@ async function hashPassword(pw: string) {
 }
 
 export async function POST(req: Request) {
-    let connection;
     const { state } = await req.json()
     console.log('state', state)
     try {
-        connection = await connectMysql()
-        // 테이블 생성 쿼리 (파라미터 바인딩을 사용하는 것이 좋습니다)
-        const insertTableQuery = `
-            INSERT INTO admin (id, pw, name, contact, email, status)
-            VALUES (?, ?, ?, ?, ?, ?);
-            `;
-
         // 쿼리 실행
-        if (connection) {
-            const hashedPassword = await hashPassword(state.pw)
-            const [result] = await connection.execute(insertTableQuery, [
-                state.id,
-                hashedPassword,
-                state.name,
-                state.contact,
-                state.email,
-                'pending',
-            ]);
-            console.log('Table admin insert successfully');
-            return NextResponse.json({ success: true, result });
-        } else {
-            console.error('Connection is undefined');
-            return NextResponse.json({ success: false, error: 'Connection is undefined' });
-        }
+        const hashedPassword = await hashPassword(state.pw)
+        const result = await sql`
+            INSERT INTO admin (id, pw, name, contact, email, status)
+             VALUES (${state.id}, ${hashedPassword}, ${state.name}, ${state.contact}, ${state.email},  'pending');
+        `;
+        console.log('Table admin insert successfully');
+        return NextResponse.json({ success: true, result });
     }
     catch (error: unknown) {
         console.error('DB 저장 error', error)
-        if ((error as any).code === 'ER_DUP_ENTRY') { return NextResponse.json({ success: false, message: '중복가입오류' }) }
+        if ((error as any).code === 23505) { return NextResponse.json({ success: false, message: '중복가입오류' }) }
         else { return NextResponse.json({ success: false, message: '관리자 등록 요청에 실패했습니다' }) }
-    }
-    finally {
-        // 연결 종료
-        if (connection) {
-            try {
-                await connection.end();
-            } catch (err) {
-                console.error('Error ending connection:', err);
-            }
-        }
     }
 }
 
 export async function GET() {
-    let connection;
     try {
-        connection = await connectMysql()
-        if (connection) {
-            const [result] = await connection.query('select * from admin')
-            console.log('get tableDB successfully')
-            return NextResponse.json({ success: true, result })
-        } else {
-            console.error('Connection is undefined');
-            return NextResponse.json({ success: false, error: 'Connection is undefined' });
-        }
+        const result = await sql.query('select * from admin')
+        console.log('get tableDB successfully')
+        return NextResponse.json({ success: true, result })
+
     } catch (error) {
         console.error('Get DBTable error', error)
         return NextResponse.json({ success: false, error })
-    } finally {
-        if (connection) {
-            try {
-                await connection.end();
-            } catch (error) {
-                console.error('Error ending connection:', error);
-            }
-        }
     }
 }
 
@@ -93,34 +50,18 @@ export async function PUT(req: Request) {
     const body = await req.json()
     const id = body.id; // ID를 정수로 변환
     const status = body.status; // 업데이트할 상태
-    console.log('body', body)
-    console.log('id', id)
-    console.log('status', status)
-   
-    let connection;
+
     try {
-        connection = await connectMysql()
-        if (connection) {
-            const [result] = await connection.query(
-                'UPDATE admin SET status = ? WHERE id = ?',
-                [status, id] // 상태와 ID를 쿼리 파라미터로 전달
-            );
-            console.log('result',result)
-            return NextResponse.json({ success: true, result })
-        } else {
-            console.error('Connection is undefined');
-            return NextResponse.json({ success: false, error: 'Connection is undefined' });
-        }
+        const result = await sql`
+        UPDATE admin
+        SET status = ${status}
+        WHERE id = ${id}
+    `;
+        console.log('result', result)
+        return NextResponse.json({ success: true, result })
+
     } catch (error) {
         console.error('PUT DBTable error', error)
         return NextResponse.json({ success: false, error })
-    } finally {
-        if (connection) {
-            try {
-                await connection.end();
-            } catch (error) {
-                console.error('Error ending connection:', error);
-            }
-        }
     }
 }
---
+++ interior/src/app/api/db/reservation/vercelPostgreCreateTable.ts
@@ -0,0 +1,24 @@
+import { sql } from '@vercel/postgres';
+
+export async function createTable() {
+    try {
+        // 테이블 생성 쿼리
+        await sql`
+      CREATE TABLE IF NOT EXISTS reservation (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            contact VARCHAR(15) NOT NULL,  
+            address VARCHAR(30) NOT NULL,    
+            type VARCHAR(15) NOT NULL,     
+            py VARCHAR(15) NOT NULL,       
+            schedule VARCHAR(15) NOT NULL, 
+            callTime VARCHAR(15) NOT NULL, 
+            qna TEXT NOT NULL,            
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `;
+        console.log('Table created successfully');
+    } catch (err) {
+        console.error('Error creating table:', err);
+    }
+}
+
